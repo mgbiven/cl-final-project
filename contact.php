dcbd24568f642a57dd6a37959794b62807e7f7b8
mgbiven <mark.biven@gmail.com>
2016-03-07 20:35:01 -0500

    I've moved my project to a new folder to support my local server. This is the first commit for this new folder. In addition to a new folder, I have added a contact form that works

---
+++ contact.php
@@ -0,0 +1,177 @@
+<?php
+	if (isset($_POST["submit"])) {
+		$name = $_POST['name'];
+		$email = $_POST['email'];
+		$message = $_POST['message'];
+		$human = intval($_POST['human']);
+		$from = 'Mid-Town Coffee Contact Us Form'; 
+		$to = 'mark.biven@gmail.com'; 
+		$subject = 'Message from Mid-Town Coffee Contact Form';
+		
+		$body ="From: $name\n E-Mail: $email\n Message:\n $message";
+		// Check if name has been entered
+		if (!$_POST['name']) {
+			$errName = 'Please enter your name';
+		}
+		
+		// Check if email has been entered and is valid
+		if (!$_POST['email'] || !filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) {
+			$errEmail = 'Please enter a valid email address';
+		}
+		
+		//Check if message has been entered
+		if (!$_POST['message']) {
+			$errMessage = 'Please enter your message';
+		}
+		//Check if simple anti-bot test is correct
+		if ($human !== 5) {
+			$errHuman = 'Your anti-spam is incorrect';
+		}
+// If there are no errors, send the email
+if (!$errName && !$errEmail && !$errMessage && !$errHuman) {
+	if (mail ($to, $subject, $body, $from)) {
+		$result='<div class="alert alert-success">Thank You for reaching out to us!</div>';
+	} else {
+		$result='<div class="alert alert-danger">Sorry there was an error sending your message. Please try again later.</div>';
+	}
+}
+	}
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head -->
+    <meta name="Mid-Town Coffee" content="Coffee Education">
+    <meta name="MG Biven" content="">
+    <link rel="icon" href="../../favicon.ico">
+
+    <title>Mid-Town Coffee</title>
+
+    <!-- CSS Links -->
+    
+    <!-- Normalize CSS -->  
+    <link rel="stylesheet" href="css/normalize.css">
+    
+    <!-- Bootstrap core CSS -->
+    <link href="css/bootstrap.css" rel="stylesheet">
+    
+    <!-- Bootstrap Carousel CSS -->
+    <link href="css/carousel.css" rel="stylesheet">
+    
+    <!-- MidTown Coffee CSS file -->
+    <link href="css/midtown.css" rel="stylesheet">
+    
+    <!-- Links CSS file -->
+    <link href="css/links.css" rel="stylesheet">
+    
+   <!-- This gets the fonts for the footer icons -->
+    <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css">
+    
+    <!-- Footer Styling -->
+    <link href="css/footer-distributed.css" rel="stylesheet">
+    
+    <!-- Tumblr CSS -->
+    <link href="css/tumblr.css" rel="stylesheet">
+
+  </head>
+
+  <body>
+
+    <!-- Header template using Angular -->
+    <div ng-app="" ng-include="'templates/header.html'"></div>
+    <div ng-view></div>
+ 
+<h1 class="header"> Have some ideas on future articles? Drop us a note!</h1>    
+    
+      <div class="container">
+ 				<form class="form-horizontal" role="form" method="post" action="contact.php">
+					<div class="form-group">
+						<label for="name" class="col-sm-2 control-label">Name</label>
+						<div class="col-sm-10">
+							<input type="text" class="form-control" id="name" name="name" placeholder="First & Last Name" value="<?php echo htmlspecialchars($_POST['name']); ?>">
+							<?php echo "<p class='text-danger'>$errName</p>";?>
+						</div>
+					</div>
+					<div class="form-group">
+						<label for="email" class="col-sm-2 control-label">Email</label>
+						<div class="col-sm-10">
+							<input type="email" class="form-control" id="email" name="email" placeholder="example@domain.com" value="<?php echo htmlspecialchars($_POST['email']); ?>">
+							<?php echo "<p class='text-danger'>$errEmail</p>";?>
+						</div>
+					</div>
+					<div class="form-group">
+						<label for="message" class="col-sm-2 control-label">Message</label>
+						<div class="col-sm-10">
+							<textarea class="form-control" rows="4" name="message"><?php echo htmlspecialchars($_POST['message']);?></textarea>
+							<?php echo "<p class='text-danger'>$errMessage</p>";?>
+						</div>
+					</div>
+					<div class="form-group">
+						<label for="human" class="col-sm-2 control-label">2 + 3 = ?</label>
+						<div class="col-sm-10">
+							<input type="text" class="form-control" id="human" name="human" placeholder="Your Answer">
+							<?php echo "<p class='text-danger'>$errHuman</p>";?>
+						</div>
+					</div>
+					<div class="form-group">
+						<div class="col-sm-10 col-sm-offset-2">
+							<input id="submit" name="submit" type="submit" value="Send" class="btn btn-primary">
+						</div>
+					</div>
+					<div class="form-group">
+						<div class="col-sm-10 col-sm-offset-2">
+							<?php echo $result; ?>	
+						</div>
+					</div>
+          </form>
+     </div>
+   
+    
+    <!-- Footer Definition --> 
+    
+       <footer class="footer-distributed">
+
+			<div class="footer-right">
+
+				<a href="#"><i class="fa fa-tumblr"></i></a>
+				<a href="#"><i class="fa fa-twitter"></i></a>
+				<a href="#"><i class="fa fa-envelope"></i></a>
+				<a href="#"><i class="fa fa-github"></i></a>
+
+			</div>
+
+			<div class="footer-left">
+
+				<p class="footer-links">
+					<a href="index.html">Home</a>
+					·
+					<a href="blog.html">Blog</a>
+					·
+					<a href="#">Education</a>
+					·
+					<a href="links.html">Coffee Links</a>
+					·
+					<a href="contact.html">Contact Us</a>
+				</p>
+
+				<p>Mid-Town Coffee &copy; 2016</p>
+			</div>
+
+		</footer>
+
+
+    
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="http://ajax.googleapis.com/ajax/libs/angularjs/1.4.8/angular.min.js"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+    <script>window.jQuery || document.write('<script src="/js/jquery.min.js"><\/script>')</script>
+    <script src="/js/bootstrap.min.js"></script>
+  </body>
+</html>
